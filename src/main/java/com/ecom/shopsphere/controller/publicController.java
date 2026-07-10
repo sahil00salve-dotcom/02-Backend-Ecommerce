a43bab33ecb33b81022ca8c20fa105159d347172
Sahil Salve <sahil00salve@gmail.com>
2026-07-10 18:02:46 +0530

    chore: initialize Spring Boot ecommerce backend

---
+++ src/main/java/com/ecom/shopsphere/controller/publicController.java
@@ -0,0 +1,12 @@
+package com.ecom.shopsphere.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class publicController {
+
+    @RequestMapping("/Health-Check")
+    public String healthcheck(){
+        return "Application is Running !";
+    }
+}
